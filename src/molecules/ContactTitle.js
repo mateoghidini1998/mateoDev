b3b1862355309fa043bc6e29161c2267046e3c43
MateoGhidini <mateo.ghidini@argano.com>
2023-02-23 17:58:38 -0300

    fixed font styles for contact header

--- src/molecules/ContactTitle.js
+++ src/molecules/ContactTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactTitle = (props) => {
   return (
-    <div className="title">
+    <div className="title" id="header" style={{width: '100%'}}>
         <h2 className="section-title">{props.title}</h2>
     </div>
   )
